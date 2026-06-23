c0ba8f007ee0823a7c1bd8f056e8b95600f5f362
minduwin <88805397+minduwin@users.noreply.github.com>
2026-06-23 09:37:11 -0400

    Update pool file with PaaS info

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,19 @@
 const { Pool } = require('pg');
+require('dotenv').config();
+
+const db_host = process.env.DB_HOST;
+const db_user = process.env.DB_USER;
+const db_sql = process.env.DB_SQL;
+const db_pw = process.env.DB_PW;
+const db_port = process.env.DB_PORT;
 
 module.exports = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    database: 'members',
-    password: '1234',
-    port: 5432
+    host: `${db_host}`,
+    user: `${db_user}`,
+    database: `${db_sql}`,
+    password: `${db_pw}`,
+    port: `${db_port}`,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000,
+    ssl: true,
 });
